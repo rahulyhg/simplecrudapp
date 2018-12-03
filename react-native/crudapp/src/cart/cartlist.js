4991d718768bbf1fc085fb079eb46b323b79ebc5
Afifah Salimi <afifahsalimi@Afifahs-MacBook-Pro.local>
2018-12-04 07:16:50 +0800

    Adding feature to show user's cart.

--- react-native/crudapp/src/cart/cartlist.js
+++ react-native/crudapp/src/cart/cartlist.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, Image, Dimensions, StyleSheet, View } from 'react-native';
+import { AsyncStorage, Text, Dimensions, StyleSheet, View, FlatList } from 'react-native';
 import { Container, Header, Content, List, ListItem, Thumbnail, Left, Body, Right, Button, Icon } from 'native-base'
 
 const width = Dimensions.get('window').width
@@ -18,38 +18,67 @@ export default class CartList extends Component {
         this.state = {
             data: [],
         }
+        this.getCartApi()
+    }
+
+    getCartApi() {
+        AsyncStorage.getItem("@userid").then((value) => {
+            fetch(`http://10.111.240.96/simplecrudapp/api/cart/show?user_id=${value}`, {
+                method : 'GET',
+            })
+            .then((response) => {
+                response.json()
+                .then((responseJson) => {
+                    if(response.status === 200) {
+                        this.setState({
+                            data: responseJson.value
+                        })
+                    }
+                    else {
+                        alert(responseJson.message)
+                    }
+                })
+            })
+            .done();
+        })
     }
 
     render() {
         return (
             <Container>
                 <Content>
-                    <List>
-                        <ListItem thumbnail>
-                        <Left>
-                            <Thumbnail square source={{ uri: 'https://placeimg.com/640/640/nature' }} />
-                        </Left>
-                        <Body>
-                            <Text style={{fontSize: 15}}>Name Name Name Name</Text>
-                            <View style={{flex: 1, flexDirection: 'row', marginTop: 5}}>
-                                <View style={{width: 25, height: 25, backgroundColor: 'lightgrey', justifyContent: 'center', alignItems: 'center'}}>
-                                    <Icon active name='ios-remove' style={{color: 'black', fontSize: 25}}/>
+                    <FlatList
+                        data={this.state.data}
+                        renderItem={({ item }) => (
+                        <List>
+                            <ListItem thumbnail>
+                            <Left>
+                                <Thumbnail square source={{ uri: 'https://placeimg.com/640/640/nature' }} />
+                            </Left>
+                            <Body>
+                                <Text style={{fontSize: 15}}>{item.name}</Text>
+                                <Text style={{fontSize: 15}}>RM{item.price}</Text>
+                                <View style={{flex: 1, flexDirection: 'row', marginTop: 5}}>
+                                    <View style={{width: 25, height: 25, backgroundColor: 'lightgrey', justifyContent: 'center', alignItems: 'center'}}>
+                                        <Icon active name='ios-remove' style={{color: 'black', fontSize: 25}}/>
+                                    </View>
+                                    <View style={{width: 50, height: 25, backgroundColor: '#f7f7f7', justifyContent: 'center', alignItems: 'center'}}>
+                                    <Text style={{fontSize: 15}}>{item.quantity}</Text>
+                                    </View>
+                                    <View style={{width: 25, height: 25, backgroundColor: 'lightgrey', justifyContent: 'center', alignItems: 'center'}}>
+                                        <Icon active name='ios-add' style={{color: 'black', fontSize: 25}}/>
+                                    </View>
                                 </View>
-                                <View style={{width: 50, height: 25, backgroundColor: '#f7f7f7', justifyContent: 'center', alignItems: 'center'}}>
-                                <Text style={{fontSize: 15}}>2</Text>
-                                </View>
-                                <View style={{width: 25, height: 25, backgroundColor: 'lightgrey', justifyContent: 'center', alignItems: 'center'}}>
-                                    <Icon active name='ios-add' style={{color: 'black', fontSize: 25}}/>
-                                </View>
-                            </View>
-                        </Body>
-                        <Right>
-                            <Button transparent>
-                                <Icon active name='ios-trash' style={{color: 'red', fontSize: 30}}/>
-                            </Button>
-                        </Right>
-                        </ListItem>
-                    </List>
+                            </Body>
+                            <Right>
+                                <Button transparent>
+                                    <Icon active name='ios-trash' style={{color: 'red', fontSize: 30}}/>
+                                </Button>
+                            </Right>
+                            </ListItem>
+                        </List>
+                    )}
+                    />
                 </Content>
             </Container>
         );
